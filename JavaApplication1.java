05da6a87e28fb749b3120068f3a782d5b67d50e4
Dzalila <70012547+Dzalila@users.noreply.github.com>
2020-08-21 19:38:50 +0700

    tugas membuat project biodata

---
+++ JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama Siswa : Dzalila Mustofa");
+        System.out.println("Jenis Kelamin : Perempuan");
+        System.out.println("Alamat : Jombor Atas");
+        System.out.println("Jurusan : Rekayasa Perangkat Lunak"); 
+    }
+    
+}
