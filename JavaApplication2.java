051598c0cf85b6dd39f28eed7f04c6f73d2d8b89
Dzalila <70012547+Dzalila@users.noreply.github.com>
2020-09-02 21:34:53 +0700

    project menghitung luas segitiga

---
+++ JavaApplication2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // menghitung luas segitiga
+        double alas = 15;
+        double tinggi = 21;
+        double luasSegitiga = 0.5*alas*tinggi;
+        System.out.println("alas = "+alas);
+        System.out.println("tinggi = "+tinggi);
+        System.out.println("Luas Segitiga Adalah = "+luasSegitiga);
+    }
+    
+}
