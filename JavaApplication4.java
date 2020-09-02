9eb737d2dcd719b394e5041c24cdc462baf19ee0
Dzalila <70012547+Dzalila@users.noreply.github.com>
2020-09-02 21:37:37 +0700

    project menghitung luas persegi panjang

---
+++ JavaApplication4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //menghitung luas persegi panjang
+        int panjang = 20;
+        int lebar = 17;
+        int luasPersegiPanjang = panjang*lebar;
+        System.out.println("panjang = "+panjang);
+        System.out.println("lebar = "+lebar);
+        System.out.println("Luas Persegi Panjang Adalah = "+luasPersegiPanjang);
+    }
+    
+}
