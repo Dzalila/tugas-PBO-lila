3964a46f63955e9e84426629eb3c1211c024b87a
Dzalila <70012547+Dzalila@users.noreply.github.com>
2020-09-02 20:40:29 +0700

    project menghitung luas lingkaran

---
+++ LuasLingkaran.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menghitung;
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class LuasLingkaran {
+    public static void main(String[] args) {
+        double jarijari = 6;
+        double phi = 3.14;
+        double luasLingkaran = phi*jarijari*jarijari;
+        System.out.println("jarijari = "+jarijari);
+        System.out.println("phi = "+phi);
+        System.out.println("Luas Lingkaran Adalah = "+luasLingkaran);
+        
+    }
+    
+}
