a6cb03892c56be86c8451a44cf6b82420098f64e
Dzalila <70012547+Dzalila@users.noreply.github.com>
2020-10-16 10:54:20 +0700

    project membuat biodata menggunakan class

---
+++ JavaApplication7/src/Biodata.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class Biodata {
+    public void namaLengkap() {
+   System.out.println("Nama : Dzalila Mustofa");
+}
+    public void Kelas() {
+   System.out.println("Kelas : XI RPL 1");
+}
+    public void noAbsen() {
+   System.out.println("No Absen : 09");
+}
+    public void Jurusan() {
+   System.out.println("Jurusan : Rekayasa Prangkat Lunak");
+}
+    public void Sekolah() {
+   System.out.println("Sekolah : SMKN 1 Purwosari");
+}
+    public void Ttl() {
+   System.out.println("Ttl : Pasuruan, 23 November 2003");
+}
+    public void jenisKelamin() {
+   System.out.println("JenisKelamin : Perempuan");
+}
+    public void Umur() {
+   System.out.println("Umur : 16 tahun");
+}
+    public void Alamat() {
+   System.out.println("Alamat : Jombor Atas, Sukorejo");
+}
+    public void Hobi() {
+   System.out.println("Hobi : Menyanyi, Traveling");
+}
+    
+}
---
+++ JavaApplication7/src/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DZALILA MUSTAFA
+ */
+public class Main {
+    public static void main(String[] args) {
+        Biodata biodata = new Biodata();
+        
+        biodata.namaLengkap();
+        biodata.Kelas();
+        biodata.noAbsen();
+        biodata.Jurusan();
+        biodata.Sekolah();
+        biodata.Ttl();
+        biodata.jenisKelamin();
+        biodata.Umur();
+        biodata.Alamat();
+        biodata.Hobi();
+    }
+}
